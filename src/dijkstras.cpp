8c182345366429035007131224460078bd52ae45
maalema2 <maalema2@uci.edu>
2025-03-11 01:48:46 +0000

    printing errors fix

--- src/dijkstras.cpp
+++ src/dijkstras.cpp
@@ -43,10 +43,11 @@ vector<int> extract_shortest_path(const vector<int>& distances, const vector<int
 }
 
 void print_path(const vector<int>& v, int total) {
-    for (size_t i = 0; i < v.size(); i++) {
-        cout << v[i];
-        if (i < v.size() - 1) cout << " -> ";
+    for (size_t i = 0; i < path.size(); i++) {
+        cout << path[i];
+        if (i < path.size() - 1) cout << " ";
     }
-    cout << "\nTotal cost: " << total << endl;
+
+    cout << "\nTotal cost is " << total << endl;
 }
 
